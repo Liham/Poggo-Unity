b70c325d67633cf6def99b017ab09ed827e795a1
Liham <atypicallpcs@gmail.com>
2019-02-08 07:57:41 -0500

    Create PlayerMelee.cs

---
+++ PlayerMelee.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMelee : MonoBehaviour
+{
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyScript>().TakeDamage(damage);
+                }
+                timeBtwAttack = startTimeBtwAttack;
+            }
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
