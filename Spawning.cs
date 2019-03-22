03639a1a241bc5c4dcc7af4d0232d6e2546720b3
Liham <atypicallpcs@gmail.com>
2019-03-22 08:57:31 -0400

    Update Spawning.cs

--- Spawning.cs
+++ Spawning.cs
@@ -5,18 +5,52 @@ using UnityEngine;
 public class Spawning : MonoBehaviour
 {
 
-    private int playerRandX;
-    private int playerRandY;
+    private int RandX;
+    private int RandY;
     private Vector3 randPosition;
+    private Vector3 randRayPosition;
     public Transform playerPrefab;
 
+    private float correctDist;
+
+    private bool spawned = false;
+
+   
     void Start()
     {
-        playerRandX = Random.Range(0, 64);
-        playerRandY = Random.Range(0, 36);
-        randPosition = new Vector3(playerRandX, playerRandY, 0);
+        while (!spawned)
+        {
+            RandX = Random.Range(-64, 64);
+            RandY = Random.Range(-36, 36);
+            randPosition = new Vector3(RandX, RandY, 0);
+            randRayPosition = new Vector3(RandX, RandY, -10);
 
-        Instantiate(playerPrefab, randPosition, Quaternion.identity);
+            RaycastHit checkSpawnPoint;
+            Ray checkingRay = new Ray(randRayPosition, Vector3.forward);
+
+            if (Physics.Raycast(checkingRay, out checkSpawnPoint))
+            {
+                Debug.DrawRay(randRayPosition, Vector3.forward, Color.red, 1000000);
+                if (checkSpawnPoint.distance > 10)
+                {
+                    Debug.Log("Hit the plane");
+                    Instantiate(playerPrefab, randPosition, Quaternion.identity);
+                    spawned = true;
+                }
+                else
+                {
+                    Debug.Log("Hit the mesh");
+                }
+            }
+        }
+        
+    }
+
+
+    void Update()
+    {
+
+
     }
 
 }
